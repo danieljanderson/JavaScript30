b5f0e5b3903117596a5fb4eac77a60017ab4f85a
Daniel <danieljanderson0616@gmail.com>
2018-06-04 15:41:45 -0400

    sounds play

--- Drumkit/app.js
+++ Drumkit/app.js
@@ -1,5 +1,9 @@
 (function(){
     window.addEventListener('keydown',function(e){
-      console.log(e)
+      const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`)
+      console.log(audio)
+      //stops the function from running all together
+      if(!audio)return
+      audio.play()
       })
 }())
