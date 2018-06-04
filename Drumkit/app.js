f1f0b775cdaa16529138dda909f0f6035c6dddf7
Daniel <danieljanderson0616@gmail.com>
2018-06-04 16:27:21 -0400

    added background and transitions

--- Drumkit/app.js
+++ Drumkit/app.js
@@ -1,9 +1,23 @@
 (function(){
-    window.addEventListener('keydown',function(e){
-      const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`)
-      console.log(audio)
-      //stops the function from running all together
-      if(!audio)return
+  function playSound(e){
+    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    //stops the function from running all together
+    if(!audio)return
+    // this will allow you to play sounds multiple times.
+      audio.currentTime = 0
       audio.play()
-      })
+      key.classList.add('playing')
+
+    }
+  function removeTransition(e){
+    //skip it if it's not a transform
+    if(e.propertyName !=='transform')return
+      this.classList.remove('playing')
+      return
+
+    }
+  const keys= document.querySelectorAll('.key')
+  keys.forEach(key => key.addEventListener('transitionend',removeTransition))
+  window.addEventListener('keydown',playSound)
 }())
