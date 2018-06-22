92012c7db734fa3d679d270ffe70dccfda3c1b0b
Daniel <danieljanderson0616@gmail.com>
2018-06-22 13:10:40 -0400

    color and blur work

--- CSS_and_JS_Variables/app.js
+++ CSS_and_JS_Variables/app.js
@@ -1,6 +1,12 @@
 var cssVariables = (function(){
     var functionVariables = function(){
-       
+        const inputs = document.querySelectorAll('.controls input')
+        function handleUpdate(){
+            const suffix = this.dataset.sizing || ''
+            document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix)
+        }
+     inputs.forEach(input => input.addEventListener('change',handleUpdate))
+     inputs.forEach(input => input.addEventListener('mousemove',handleUpdate))
     }
 var module ={
     'functionVariables':functionVariables
